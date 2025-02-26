c0a174d92c4f8de9f575e40fc818ba8bad01de8d
DineniDaundasekara <daundasekaradineni@gmail.com>
2025-02-26 12:34:50 +0530

    add description to homepage

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -34,8 +34,8 @@ const Header = ({ scrollToProducts }) => {
       </video>
       
       <div className="header-contents">
-        <h2>Build Your Dreams, One Tool at a Time</h2>
-        <p>Explore our wide range of quality tools and hardware to make your projects a success. Whether you're a DIY enthusiast or a professional builder, find everything you need at Kandurata Hardware. Start building today!</p>
+        <h2>Bringing timeless beauty to every space</h2>
+        <p>Explore our wide range of premium furniture to transform your space. Whether you're looking for elegant home décor or durable office furniture, find everything you need at FERNIHAVEN. Start creating your perfect space today!</p>
         <button onClick={scrollToProducts}>View Shop</button>
       </div>
     </div>
